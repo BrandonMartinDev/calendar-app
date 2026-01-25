e795c4e13d60ce29703b2a12e1a7b918e8082a8d
Brandon Martin <SomeBusinessEmail@protonmail.com>
2026-01-25 18:06:43 -0500

    Created basic routing

---
+++ server/src/api/routers/main.router.ts
@@ -0,0 +1,57 @@
+// -- == [[ IMPORTS ]] == -- \\
+
+// Types
+
+import {
+    type Request,
+    type Response,
+    type NextFunction
+} from "express";
+
+
+// Modules
+
+import express from 'express';
+
+
+// Utils
+
+import HandleError from 'api/utils/handleError.util';
+import SendResponse from "api/utils/sendResponse.util";
+
+
+
+// -- == [[ INITIALIZE MAIN ROUTER ]] == -- \\
+
+const MainRouter = express.Router();
+
+
+
+// -- == [[ ROUTE ENDPOINTS TO ROUTERS ]] == -- \\
+
+
+
+
+// -- == [[ HANDLE CATCH-ALL ENDPOINTS ]] == -- \\
+
+MainRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        SendResponse(res, {
+            statusCode: 200,
+            responseJson: {
+                message: "/api/v1/ is OK"
+            }
+        });
+
+    } catch (error) {
+        HandleError(undefined, { error: error });
+    }
+
+});
+
+
+// -- == [[ EXPORTS ]] == -- \\
+
+export default MainRouter;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,34 @@
 // -- == [[ IMPORTS ]] == -- \\
 
 // Obligatory dotenv import
-
 import "dotenv/config";
 
 
-// Config
+// Types
 
+import {
+    type Request,
+    type Response,
+    type NextFunction
+} from "express";
+
+
+// Config
 import { PORT } from "@config/defaults.config";
 
 
 // Express
-
 import express from "express";
+
+
+// Routers
+import MainRouter from "@routers/main.router";
+
+
+// Utils
+
+import SendResponse from "api/utils/sendResponse.util";
+import HandleError from "api/utils/handleError.util";
 
 
 
@@ -21,10 +37,50 @@ import express from "express";
 const app = express();
 
 
+app.use("/api/v1", MainRouter);
+
+
+
+// -- == [[ HANDLE CATCH-ALL ENDPOINTS ]] == -- \\
+
+app.get("/", (req: Request, res: Response) => {
+
+    try {
+
+        SendResponse(res, {
+            statusCode: 200,
+            responseJson: {
+                message: "Server is OK"
+            }
+        })
+
+    } catch (error) {
+        HandleError(res, { error: error });
+    }
+
+});
+
+
+app.all("/*splat", (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        SendResponse(res, {
+            statusCode: 404,
+            responseJson: {
+                message: (`Requested resource '${req.method} ${req.originalUrl}' could not be found`)
+            }
+        })
+
+    } catch (error) {
+        HandleError(res, { error: error });
+    }
+
+});
+
 
 
 // -- == [[ INITIALIZE SERVER ]] == -- \\
-
 
 const main = async () => {
 
@@ -32,10 +88,10 @@ const main = async () => {
 
         app.listen(PORT, () => {
             console.warn("CALENDAR APP SERVER SUCCESSFULLY LISTENING ON PORT:", PORT);
-        })
+        });
 
     } catch (error) {
-
+        HandleError(undefined, { error: error });
     }
 
 }
