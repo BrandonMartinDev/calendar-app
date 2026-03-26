3f4f695c54f2e2ca95dc5b9e38a62fd9d5fdfa77
Brandon Martin <SomeBusinessEmail@protonmail.com>
2026-03-25 20:40:15 -0400

    Created dates util

---
+++ shared/utils/dates.utils.ts
@@ -0,0 +1,57 @@
+// -- == [[ IMPORTS ]] == -- \\
+
+// Types
+
+import type { DayNumber, MonthNumber } from "@shared/types/main";
+
+
+
+// -- == [[ METHODS ]] == -- \\
+
+const getDayString = (day: DayNumber) => {
+
+    switch (day) {
+
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+
+    }
+
+}
+
+const getFirstDateInMonth = (year: number, month: MonthNumber) => {
+    return new Date(year, month, 1);
+}
+
+const getLastDateInMonth = (year: number, month: MonthNumber) => {
+    return new Date(year, month + 1, 0);
+}
+
+const getDaysInMonth = (year: number, month: MonthNumber) => {
+    return new Date(year, month + 1, 0).getDate();
+}
+
+
+
+// -- == [[ EXPORTS ]] == -- \\
+
+export {
+
+    getDayString,
+    getFirstDateInMonth,
+    getLastDateInMonth,
+    getDaysInMonth
+
+}
