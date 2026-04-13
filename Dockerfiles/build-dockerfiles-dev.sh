0ca7b7d783729a5ac4632ba8d76a1a34b68f104e
Brandon Martin <SomeBusinessEmail@protonmail.com>
2026-04-12 21:09:27 -0400

    Refactor

--- Dockerfiles/build-dockerfiles-dev.sh
+++ Dockerfiles/build-dockerfiles-dev.sh
@@ -1,20 +1,40 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo ""
+    echo "Please provide a tag! Example Usage: 'build-dockerfiles-dev.sh <string>'"
+    echo ""
+    exit;
+fi
+
 clear;
-
-echo "";
-echo "Building server";
+echo "Building server | 'calendar-server:$1'";
 echo "";
 
-docker build -f ./Dockerfile.server -t calendar-server ../ --no-cache
+
+echo "Starting In..";
+
+for i in {1..3}; do
+    echo "$(expr 3 - $i + 1)..."
+    sleep 1s
+done;
+
+docker build -f ./Dockerfile.server -t calendar-server:$1 ../
 
 echo "";
 
+
+
+echo""
+echo "Building client | 'calendar-client:$1'";
+echo""
 
 sleep 2s;
 
-echo""
-echo "Building client"
-echo""
+docker build -f ./Dockerfile.client \
+--build-arg VITE_IS_PROD=true \
+--build-arg VITE_PROD_BACKEND_URL=http://localhost:8080 \
+--build-arg PROD_FRONTEND_URL=http://localhost:80 \
+-t calendar-client:$1 ../
 
-docker build -f ./Dockerfile.client -t calendar-client ../ --no-cache
-
-echo""
+echo ""
